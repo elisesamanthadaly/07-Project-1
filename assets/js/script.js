352dd38e18830bcf2e5a535649162a7ea27e5a0f
Elise Daly <elise.samantha.daly@gmail.com>
2021-03-20 13:54:27 -0400

    Added more js comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-// giphy key : aWmCF38aWZNhbJoi236yO2yuW4smGy9q
-// url "https://api.giphy.com/v1/" 
-
 $(document).ready(function() {
     var textNameEl = $("#displayName");
     var dogImageEl = $("#displayImg");
@@ -24,17 +21,17 @@ $(document).ready(function() {
     fetchName();
     fetchDog();
 
-    //Click to get new random name
+    // Click to get new random name
     $("#btnName").on("click", function() {
     fetchName();
     })
 
-    //Click to get new dog img
+    // Click to get new dog img
     $("#btnImage").on("click", function() {
         fetchDog();
     })
 
-    //Click to get new combo
+    // Click to get new combo
     $("#newCombo").on("click", function() {
         fetchName();
         fetchDog();
@@ -49,10 +46,10 @@ $(document).ready(function() {
             })
             .then(function(data){
                 var names = $("<h4>");
-                names.text(data[0])
+                names.text(data[0]);
                 textNameEl.append(names);
 
-                capturedDogName = data[0];//Captures current dogName
+                capturedDogName = data[0];// Captures current dogName
             })
     }
 
@@ -70,48 +67,54 @@ $(document).ready(function() {
                 dogImg.height(450);
                 dogImageEl.append(dogImg);
 
-                capturedDogURL = dogImg.attr("src");//Captures current dogURL
+                capturedDogURL = dogImg.attr("src");// Captures current dogURL
             })
     }
     
-    //Click to add current dog name/URL combo to favorites
+    // Click to add current dog name/URL combo to favorites
     $("#btnSave").click(function() {
+        // Prevents saving current combo to favorites multiple times
         if ((repeatCheckName === capturedDogName) && (repeatCheckURL === capturedDogURL)) {
             return;
         }
 
+        // Retrieves captured values
         favorite.dogName = capturedDogName;
         favorite.dogURL = capturedDogURL;
 
+        // Retrieves stored favorites
         storedFavorites = JSON.parse(localStorage.getItem("favorites"));
-    
         if (storedFavorites !== null) {
             favorites = storedFavorites;
         }
 
+        // Pushes new favorite to favorites array
         favorites.push(favorite);
-
         localStorage.setItem("favorites", JSON.stringify(favorites));
 
+        // Sets current combo to be checked for duplicity if save button hit again
         repeatCheckName = favorites[favorites.length - 1].dogName;
         repeatCheckURL = favorites[favorites.length - 1].dogURL;
     });
 
-    //Click to clear out favorites list
+    // Click to clear out favorites list
     $("#btnClear").click(function() {
         favorites = [];
         localStorage.setItem("favorites", JSON.stringify(favorites));
 
+        // Resets carousel
         carouselIndex = -1;
         $("#currentFavName").text("");
         $("#currentFavImage").attr("src", "./assets/images/spinning_doge.png");
 
+        // Resets duplicate variables
         repeatCheckName = "";
         repeatCheckURL = "";
     });
 
     // Click to cycle backwards through favorites array
     $("#btnPrev").click(function() {
+        // Retrieves stored favorites
         storedFavorites = JSON.parse(localStorage.getItem("favorites"));
         if (storedFavorites !== null) {
             favorites = storedFavorites;
@@ -119,10 +122,10 @@ $(document).ready(function() {
 
         if (favorites.length !== 0) {
             if (carouselIndex > 0) {
-                carouselIndex--;
+                carouselIndex--;// Carousel decrements backwards through array
             }
             else if (carouselIndex <= 0) {
-                carouselIndex = (favorites.length - 1);
+                carouselIndex = (favorites.length - 1);// Beyond start of array, [0], returns to last item in array
             }
 
             $("#currentFavName").text(favorites[carouselIndex].dogName);
@@ -132,6 +135,7 @@ $(document).ready(function() {
 
     // Click to cycle forwards through favorites array
     $("#btnNext").click(function() {
+        // Retrieves stored favorites
         storedFavorites = JSON.parse(localStorage.getItem("favorites"));
         if (storedFavorites !== null) {
             favorites = storedFavorites;
@@ -139,10 +143,10 @@ $(document).ready(function() {
 
         if (favorites.length !== 0) {
             if (carouselIndex < (favorites.length - 1)) {
-                carouselIndex++;
+                carouselIndex++;// Carousel increments forwards through array
             }
             else if (carouselIndex === (favorites.length - 1)) {
-                carouselIndex = 0;
+                carouselIndex = 0;// At end of array, returns to first item in array
             }
 
             $("#currentFavName").text(favorites[carouselIndex].dogName);
